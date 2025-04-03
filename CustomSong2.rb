4c545b56cbfd083717acb33385163105de23f1f7
Zay-A <zaydan_alim@student.davincischools.org>
2025-04-03 11:44:31 -0700

    Update CustomSong2.rb

--- CustomSong2.rb
+++ CustomSong2.rb
@@ -2,7 +2,7 @@
 use_bpm 95
 use_synth :piano
 
-define :tokyo do |additionalNote, additionalSleep, s1|
+define :tokyo do |s1, additionalNote, additionalSleep, firstNoteOfTheNextMeasure, firstSleepOfTheNextMeasure|
   play :r
   sleep 0.5
   play :Bb4
@@ -17,44 +17,40 @@ define :tokyo do |additionalNote, additionalSleep, s1|
   sleep 0.5
   play :Bb4
   sleep 0.5
-  play :E4
+  play :Eb4
   sleep 0.25
-  play :D4
+  play :Db4
   sleep s1
   play additionalNote
   sleep additionalSleep
+  play firstNoteOfTheNextMeasure
+  sleep firstSleepOfTheNextMeasure
 end
 define :tokyo2 do
-  
-  
-  play :F4
-  sleep 0.75
   play :F4
   sleep 0.25
   play :Gb4
   sleep 0.25
   play :F4
   sleep 0.25
-  play :D4
+  play :Db4
   sleep 0.5
-  play :B3
+  play :Bb3
   sleep 0.5
   play :r
   sleep 0.25
-  play :B3
+  play :Bb3
   sleep 0.25
-  play :B3
+  play :Bb3
   sleep 0.25
-  play :E4
+  play :Eb4
   sleep 0.25
-  play :D4
+  play :Db4
   sleep 0.5
-  play :E4
+  play :Eb4
   sleep 0.25
 end
 define :tokyo3 do
-  play :D4
-  sleep 1
   play :F4
   sleep 1
   play :Gb4
@@ -65,7 +61,7 @@ define :tokyo3 do
   sleep 0.25
   play :E4
   sleep 0.25
-  play :D4
+  play :Db4
   sleep 0.5
   play :E4
   sleep 0.25
@@ -81,32 +77,36 @@ end
 
 live_loop :tokyovania do
   5.times do
-    tokyo :eb4,0.25
+    tokyo 0.5, :Fb4, 0.75, :r , 0
     tokyo2
-    tokyo :r,0.25
+    tokyo  0.75, :r, 0 , :D4, 1
     tokyo3
   end
   stop
 end
-live_loop
-tokyoBass :Cb3,:Cb4, 1
-tokyoBass :Cb3,:r, 1
-tokyoBass :Db3,:r, 1
-tokyoBass :Db3,:r, 1
-tokyoBass :Bb2, :r, 1
-tokyoBass :Bb2, :r, 1
-tokyoBass :Eb3, :r, 1
-tokyoBass :Eb3, :r, 0.5
-tokyoBass :Db3, :r, 0.5
-tokyoBass :Cb3, :Eb3, 1
-tokyoBass :Cb3, :Eb3, 1
-tokyoBass :Db3, :F3, 1
-tokyoBass :Db3, :F3, 1
-tokyoBass :Bb2, :Db3, 1
-tokyoBass :Bb2, :Db3, 1
-tokyoBass :E3, :r, 1
-tokyoBass :E3, :r, 0.5
-tokyoBass :Db3, :r, 0.5
+live_loop :tokyovaniaLow do
+  5.times do
+    tokyoBass :Cb3,:Cb4, 1
+    tokyoBass :Cb3,:r, 1
+    tokyoBass :Db3,:r, 1
+    tokyoBass :Db3,:r, 1
+    tokyoBass :Bb2, :r, 1
+    tokyoBass :Bb2, :r, 1
+    tokyoBass :Eb3, :r, 1
+    tokyoBass :Eb3, :r, 0.5
+    tokyoBass :Db3, :r, 0.5
+    tokyoBass :Cb3, :Eb3, 1
+    tokyoBass :Cb3, :Eb3, 1
+    tokyoBass :Db3, :F3, 1
+    tokyoBass :Db3, :F3, 1
+    tokyoBass :Bb2, :Db3, 1
+    tokyoBass :Bb2, :Db3, 1
+    tokyoBass :E3, :r, 1
+    tokyoBass :E3, :r, 0.5
+    tokyoBass :Db3, :r, 0.5
+  end
+  stop
+end
 
 
 sleep 10
