fcc663cfcb1fd0247b62285a066ba56bc6a45aeb
Zay-A <zaydan_alim@student.davincischools.org>
2025-04-11 11:12:26 -0700

    Update CustomSong2.rb

--- CustomSong2.rb
+++ CustomSong2.rb
@@ -4,40 +4,55 @@ use_bpm 95
 use_synth :piano
 ink = "C:/Users/zaydan_alim/Downloads/Ink!sans.wav"
 
-define :tokyoEnd do |x, y|
-  1.times do
+cat = "C:/Users/zaydan_alim/Downloads/ooeeaaeecat.wav"
+
+define :tokyoEnd do
+  11.times do
+    
+    
+    
     numList = [ :E4, :E4, :Eb5,:Bb4, :r, :A4, :Ab4, :G4, :E4, :G4, :A4 ]
-    numList2 = [ 0.25, 0.25, 0.5, 0.5, 0.25, 0.5, 0.5, 0.5, 0.25 ,0.25, 0.25 ]
+    numListSleep = [ 0.25, 0.25, 0.5, 0.5, 0.25, 0.5, 0.5, 0.5, 0.25 ,0.25, 0.25 ]
     notes = 0
     notes2 = 0
+    p2 = 1
     
     live_loop :tokyo1 do
-      11.times do
-        play (numList[notes]), amp: x
-        sleep (numList2[notes2])
-        notes = notes + 1
-        notes2 = notes2 + 1
-        print(notes)
+      4.times do
+        11.times do
+          play (numList[notes]), amp: p2
+          sleep (numListSleep[notes2])
+          notes = notes + 1
+          notes2 = notes2 + 1
+          p2 = p2 - 0.05
+          print(p2)
+        end
+        notes = 0
+        notes2 = 0
       end
-      notes = 0
-      notes2 = 0
     end
     
     numList3 = [ :Ab2, :Ab2, :Bb2,:Bb2, :Ab2, :Ab2, :r, :Eb3, :E2, :Eb3, :Eb3, :E2, :E2, :r ]
     numList4 = [ 1, 1, 1, 1, 1, 0.5, 0.25, 0.25, 0.5 ,0.25, 0.25, 0.5, 0.25, 0.25 ]
-    notes1 = 0
-    notes3 = 0
+    notesB = 0
+    notesA = 0
+    p = 1
     
     live_loop :tokyo1bass do
-      14.times do
-        play (numList3[notes1]), amp: y
-        sleep (numList4[notes3])
-        notes1 = notes1 + 1
-        notes3 = notes3 + 1
-        print(notes1)
+      2.times do
+        14.times do
+          play (numList3[notesA]), amp: p
+          sleep (numList4[notesB])
+          notesA = notesA + 1
+          notesB = notesB + 1
+          p = p - 0.05
+          
+          print(notesA)
+          print(notesB)
+        end
+        notesB = 0
+        notesA = 0
       end
-      notes1 = 0
-      notes3 = 0
     end
   end
 end
@@ -184,16 +199,9 @@ live_loop :bede do
   stop
 end
 
-sleep 15
 
-tokyoEnd 0.9, 0.9
-tokyoEnd 0.8, 0.8
-tokyoEnd 0.7, 0.7
-tokyoEnd 0.6, 0.6
-tokyoEnd 0.5, 0.5
-tokyoEnd 0.5, 0.5
-tokyoEnd 0.4, 0.4
-tokyoEnd 0.3, 0.3
-tokyoEnd 0.2, 0.2
-tokyoEnd 0.1, 0.1
-tokyoEnd 0, 0
+sleep 16
+
+sample cat, amp: 4
+
+tokyoEnd
